37210ff09571c7169cb0a0a22809727cde4634ad
luanaparra <luana.silva@sou.inteli.edu.br>
2023-12-04 12:21:50 -0300

    last update ponderada5

--- ponderada5/chat/chat/llm.py
+++ ponderada5/chat/chat/llm.py
@@ -1,6 +1,9 @@
+#! /bin/env python3
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg import String
+from std_msgs.msg import Float32MultiArray
+import re
+
 from langchain.chat_models import ChatOpenAI
 from langchain.prompts import ChatPromptTemplate
 from langchain.schema.runnable import RunnablePassthrough
@@ -9,32 +12,30 @@ from langchain.text_splitter import CharacterTextSplitter
 from langchain.vectorstores import FAISS
 from langchain.embeddings.openai import OpenAIEmbeddings
 from dotenv import load_dotenv
+import gradio as gr
+
 
 class LLMNode(Node):
     def __init__(self):
         super().__init__('llm_node')
-        self.subscription_ = self.create_subscription(
-            msg_type=String,
-            topic="/llm",
-            callback=self.listener_callback,
-            qos_profile=10
-        )
-        self.publisher_ = self.create_publisher(
-            msg_type=String,
-            topic="/chatbot",
-            qos_profile=10
-        )
 
-        try:
-            self.load()
-            self.get_logger().info("LLM Node created successfully")
-        except Exception as e:
-            self.get_logger().error(f"Error during initialization: {e}")
-            self.destroy_node()
-            raise
+        self.pattern = r"\(\s*\d+(\.\d+)?\s*,\s*\d+(\.\d+)?\s*\)"
+
+        self.publisher_points = self.create_publisher(
+            msg_type = Float32MultiArray,
+            topic = '/waypoints',
+            qos_profile=10)
+
+        self.load()
+
+        self.run()
+
+        self.get_logger().info("LLM Node created successfully")
 
     def load(self):
         load_dotenv()
+
+        model = ChatOpenAI(model="gpt-3.5-turbo")
 
         loader = PyPDFLoader("./chat/chat/data/points.pdf")
         pages = loader.load_and_split()
@@ -48,15 +49,12 @@ class LLMNode(Node):
 
         retriever = vectorstore.as_retriever()
 
-        template = """Answer the question based only on the following context:
+        prompt = ChatPromptTemplate.from_template(
+        """Answer the question based on the following context:
         {context}
 
         Question: {question}
-        """
-
-        prompt = ChatPromptTemplate.from_template(template)
-
-        model = ChatOpenAI(model="gpt-3.5-turbo")
+        """)
 
         self.chain = (
             {"context": retriever, "question": RunnablePassthrough()}
@@ -64,34 +62,58 @@ class LLMNode(Node):
             | model
         )
 
-    def listener_callback(self, msg):
-        try:
-            self.get_logger().info(f"Received '{msg.data}'")
-            content = ""
-            for s in self.chain.stream(msg.data):
-                content += s.content
-            self.publish_(content)
-            self.get_logger().info(f"Response published")
-        except Exception as e:
-            self.get_logger().error(f"Error processing message: {e}")
+    def publish_command(self, bot_message):
+        matched = re.search(self.pattern, bot_message)
 
-    def publish_(self, content):
-        msg = String()
-        msg.data = content
-        self.publisher_.publish(msg)
-        self.get_logger().info(f"Published '{content}'")
+        if matched:
+            self.x = float(matched.group(1))
+            self.y = float(matched.group(2))
+
+            points = Float32MultiArray()
+            points.data.append(self.x)
+            points.data.append(self.y)
+
+            print(points)
+            
+            self.publisher_points.publish(points)
+            self.get_logger().info("Não encontrei os pontos")
+        
+        else:
+            self.get_logger().info("Não encontrei os pontos")
+
+        print(f"[RESPONSE] [CHATBOT] {bot_message}")
+        
+    def run(self):
+        with gr.Blocks() as demo:
+            chatbot = gr.Chatbot()
+            msg = gr.Textbox()
+            clear = gr.ClearButton([msg, chatbot])
+
+            def respond(message, chat_history):
+                bot_message = ''
+                for s in self.chain.stream(message):
+                    bot_message += s.content
+                    self.get_logger().info(bot_message)
+
+                self.publish_command(bot_message)
+
+                chat_history.append((message, bot_message))
+                
+                return "", chat_history
+
+            msg.submit(respond, [msg, chatbot], [msg, chatbot])
+
+        demo.launch()
 
 def main(args=None):
     rclpy.init(args=args)
-    try:
-        llm_node = LLMNode()
-        rclpy.spin(llm_node)
-    except Exception as e:
-        print(f"Error: {e}")
-    finally:
-        if 'llm_node' in locals():
-            llm_node.destroy_node()
-        rclpy.shutdown()
+    llm_node = LLMNode()
+
+    rclpy.spin(llm_node)
+
+    llm_node.destroy_node()
+    rclpy.shutdown()
+
 
 if __name__ == "__main__":
     main()
