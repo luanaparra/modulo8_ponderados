36aa0d72108be32f80eadd112aac67fc054b95fd
luanaparra <luana.silva@sou.inteli.edu.br>
2023-12-17 22:29:28 -0300

    update ponderada 8

--- ponderada8/README.md
+++ ponderada8/README.md
@@ -1,12 +1,24 @@
 # Ponderada 8
 
 ## Descrição 
-Atividade em sala de aula para implementar um perceptron capaz de ser treinado para reproduzir o comportamento das seguintes portas lógicas:
+Criação de uma aplicação que integra um tradutor de texto (pode ser baseado em LLM ou não), uma aplicação de speech-to-text e uma aplicação de text-to-speech.
 
-1. AND
-2. OR
-3. NAND
-4. XOR
+Assim, a ferramenta de terminal deve ser capaz de receber como argumento o caminho para um arquivo de áudio contendo a fala de uma pessoa. Após ler o arquivo fornecido, a aplicação deve ser capaz de transformar o áudio em texto corretamente, usar esse texto gerado para alimentar um tradutor de texto (e.g. transforma um texto do português para o inglês) e, por fim, transforma o texto traduzido em áudio novamente e reproduz para o usuário.
 
-## Observação
-Para o XOR, com um único perceptron não é possível solucionar o problema devido à não linearidade. Dessa maneira, para representar o XOR, seria necessário um modelo mais complexo, como uma rede neural multicamada.
+## Execução
+
+Em primeiro lugar, é necessário instalar todas as bibliotecas e dependências.
+
+```
+pip install googletrans SpeechRecognition gtts playsound
+```
+
+Em seguida, é preciso rodar:
+
+```
+python nome_do_arquivo.py caminho_para_o_arquivo_de_audio
+```
+Nesse caso, o 'caminho_para_o_arquivo_de_audio' é onde você quer guardar o arquivo do audio. 
+
+## Vídeo
+[link](https://github.com/luanaparra/modulo8_ponderados/blob/main/ponderada4/ponderada4.mp4)
--- ponderada8/main.py
+++ ponderada8/main.py
@@ -1,77 +1,48 @@
-import numpy as np
+import os
+import sys
+import playsound
+import speech_recognition as sr
+from googletrans import Translator
+from gtts import gTTS
 
-class Perceptron:
-    def __init__(self, input_size):
-        self.weights = np.random.rand(input_size)
-        self.bias = np.random.rand()
-        self.learning_rate = 0.1
+def transcribe_translate_speak(audio_path):
+    def transcribe_audio(audio_path):
+        recognizer = sr.Recognizer()
+        audio_file = sr.AudioFile(audio_path)
+        
+        with audio_file as source:
+            audio_data = recognizer.record(source)
+        
+        try:
+            text = recognizer.recognize_google(audio_data, language='pt-BR')
+            return text
+        except sr.UnknownValueError:
+            return "Não foi possível reconhecer a fala."
+        except sr.RequestError:
+            return "Erro na requisição."
 
-    def activation_function(self, x):
-        return 1 if x >= 0 else 0  # Função de ativação degrau
+    def translate_text(text):
+        translator = Translator()
+        translation = translator.translate(text, src='pt', dest='en')
+        return translation.text
 
-    def predict(self, inputs):
-        weighted_sum = np.dot(inputs, self.weights) + self.bias
-        return self.activation_function(weighted_sum)
+    def speak_text(text):
+        tts = gTTS(text=text, lang='en')
+        tts.save("ponderada8.mp3")
+        playsound.playsound("ponderada8.mp3")
 
-    def train(self, training_inputs, labels, epochs):
-        for epoch in range(epochs):
-            for inputs, label in zip(training_inputs, labels):
-                prediction = self.predict(inputs)
-                error = label - prediction
-                self.weights += self.learning_rate * error * inputs
-                self.bias += self.learning_rate * error
+    recognized_text = transcribe_audio(audio_path)
+    if recognized_text:
+        print("Texto reconhecido:", recognized_text)
+        translated_text = translate_text(recognized_text)
+        print("Texto traduzido:", translated_text)
+        speak_text(translated_text)
+    else:
+        print("Não foi possível reconhecer a fala no arquivo fornecido.")
 
 if __name__ == "__main__":
-    # AND
-    and_inputs = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
-    and_labels = np.array([0, 0, 0, 1])
-
-    and_perceptron = Perceptron(2)
-    and_perceptron.train(and_inputs, and_labels, epochs=100)
-
-    print("Teste AND:")
-    test_inputs = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
-    for inputs in test_inputs:
-        prediction = and_perceptron.predict(inputs)
-        print(f"Entrada: {inputs} -> Saída: {prediction}")
-
-    # OR
-    or_inputs = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
-    or_labels = np.array([0, 1, 1, 1])
-
-    or_perceptron = Perceptron(2)
-    or_perceptron.train(or_inputs, or_labels, epochs=100)
-
-    print("\nTeste OR:")
-    test_inputs = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
-    for inputs in test_inputs:
-        prediction = or_perceptron.predict(inputs)
-        print(f"Entrada: {inputs} -> Saída: {prediction}")
-
-    # NAND
-    nand_inputs = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
-    nand_labels = np.array([1, 1, 1, 0])
-
-    nand_perceptron = Perceptron(2)
-    nand_perceptron.train(nand_inputs, nand_labels, epochs=100)
-
-    print("\nTeste NAND:")
-    test_inputs = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
-    for inputs in test_inputs:
-        prediction = nand_perceptron.predict(inputs)
-        print(f"Entrada: {inputs} -> Saída: {prediction}")
-
-    #XOR (não da certo)
-    xor_inputs = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
-    xor_labels = np.array([0, 1, 1, 0])
-    
-    xor_perceptron = Perceptron(2)
-    xor_perceptron.train(xor_inputs, xor_labels, epochs=100)
-
-    print("\nTeste XOR:")
-    test_inputs = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
-    for inputs in test_inputs:
-        prediction = xor_perceptron.predict(inputs)
-        print(f"Entrada: {inputs} -> Saída: {prediction}")
-
-
+    if len(sys.argv) != 2:
+        print("python nome_do_arquivo.py caminho_para_o_arquivo_de_audio")
+    else:
+        audio_file_path = sys.argv[1]
+        transcribe_translate_speak(audio_file_path)
